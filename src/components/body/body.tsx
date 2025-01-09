b23e15da8af0a92fcea64dd4a149afad8d154878
emscawas <cawasems@gmail.com>
2025-01-09 21:46:38 +0800

    updated percentage textbox: now has percent text and will not exceed to 100, put both textboxes to each container to maintain position: relative to textbox only

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import './body.css';
+import React, { useState } from "react";
+import "./body.css";
 
 function BudgetPlanner() {
-  const [income, setIncome] = useState('');
-  const [percentage, setPercentage] = useState('');
+  const [income, setIncome] = useState("");
+  const [percentage, setPercentage] = useState("");
 
   const handleIncomeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.replace(/,/g, '');
+    const value = e.target.value.replace(/,/g, "");
     if (!isNaN(Number(value))) {
       setIncome(Number(value).toLocaleString());
     }
@@ -14,35 +14,38 @@ function BudgetPlanner() {
 
   const handlePercentageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.replace(/%/g, '');
-    if (!isNaN(Number(value)) || value === '') {
-      setPercentage(value ? `${value}%` : '');
+    if (!isNaN(Number(value)) && Number(value) <= 100 || value === '') {
+      setPercentage(value);
     }
   };
 
   return (
     <div className="body">
       {/* make this component for budgetting forms */}
-      <form className='budget-form'>
-        <input
-          type="text"
-          className='income-text'
-          placeholder=''
-          value={income}
-          onChange={handleIncomeChange}
-          maxLength={12}
-        />
-        <span className='income-placeholder'>Total Income</span>
-        <input
-          type="text"
-          className='percentage-text'
-          placeholder=''
-          value={percentage}
-          onChange={handlePercentageChange}
-          maxLength={3}
-        />
-        <span className='percentage-placeholder'>Percentage</span>
+      <form className="budget-form">
+        <div className="income-container">
+          <input
+            type="text"
+            className="income-text"
+            placeholder=""
+            value={income}
+            onChange={handleIncomeChange}
+            maxLength={12}
+          />
+          <span className="income-placeholder">Total Income</span>
+        </div>
+        <div className="percentage-container">
+          <input
+            type="text"
+            className="percentage-text"
+            placeholder=""
+            value={percentage}
+            onChange={handlePercentageChange}
+            maxLength={3}
+          />
+          <span className="percentage-placeholder">Percentage</span>
+        </div>
       </form>
-
       {/* another component after this for list of budgets allocated */}
     </div>
   );
